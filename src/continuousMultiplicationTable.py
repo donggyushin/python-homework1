ff44b1682045fcf48da6c099dd38c8caf9cb330d
rontend <donggyu9410@gmail.com>
2019-03-22 17:13:33 +0900

    continuous multiplication table

---
+++ src/continuousMultiplicationTable.py
@@ -0,0 +1,32 @@
+# Make a continuous multiplication table calculator program with using for and
+# conditional statement we have learned
+# The rules of program are same as below.
+
+# print message 'Which level of multiplication table do you want to start?' after start.
+# User enters the number user wants to play
+# Program prints result of multiplication table with message 'calculate n-th multiplication table'
+# This program is different with previous example,
+# If user enters 0, then program should be ended. But user didn't enter 0, then program
+# will not be ended.
+
+
+def multiplicationTable(number):
+    print('calculate {}-th multiplication table'.format(number))
+    for i in range(10):
+        print('{} x {} = {}'.format(number, i, number * i))
+
+
+def main():
+    levelOfMultiplication = int(1)
+    while levelOfMultiplication is not 0:
+        print('which level of multiplication table do you want?(1~9)?')
+        levelOfMultiplication = int(input())
+        if levelOfMultiplication is 0:
+            break
+        multiplicationTable(levelOfMultiplication)
+    print('Quit program')
+    return
+
+
+if __name__ == '__main__':
+    main()
